bad09ffaeccc9416af965ce9d60231c339dc6d7a
Abeer Noor <abeer.noor901@gmail.com>
2025-05-02 20:55:16 +0500

    updates

--- api.py
+++ api.py
@@ -1,8 +1,13 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 import speech_recognition as sr
 import requests
+import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER = 'static/audios'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @app.route('/speech-to-text-url', methods=['POST'])
 def transcribe_audio_url():
@@ -21,6 +26,28 @@ def transcribe_audio_url():
             return jsonify({"text": text})
         except:
             return jsonify({"text": "Could not transcribe"}), 500
+
+@app.route('/upload-audio', methods=['POST'])
+def upload_audio():
+    if 'audio' not in request.files:
+        return jsonify({'error': 'No file uploaded'}), 400
+
+    file = request.files['audio']
+    if file.filename == '':
+        return jsonify({'error': 'Empty filename'}), 400
+
+    filename = secure_filename(file.filename)
+    save_path = os.path.join(UPLOAD_FOLDER, filename)
+    file.save(save_path)
+
+    host = request.host_url.rstrip('/')
+    public_url = f"{host}/static/audios/{filename}"
+
+    return jsonify({'url': public_url})
+
+@app.route('/static/audios/<path:filename>')
+def serve_audio(filename):
+    return send_from_directory(UPLOAD_FOLDER, filename)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
